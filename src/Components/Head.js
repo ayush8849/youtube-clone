c01f602de2200cff00da210d972ab11c15580b40
ayush8849 <ayushsrivastava8849@gmail.com>
2024-04-25 00:53:21 +0530

    Add toggle button

--- src/Components/Head.js
+++ src/Components/Head.js
@@ -2,24 +2,31 @@ import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { FaSearch } from "react-icons/fa";
 import { FaRegUser } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
 
-
-import { useDispatch } from "react-redux";
-import { toggleMenu } from "./utils/appSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleMenu, closeMenu } from "./utils/appSlice";
 
 const Head = () => {
   const dispatch = useDispatch();
-const toggleMenuHandler = ()=>{
-  dispatch(toggleMenu());
-};
+  const isMenuOpen = useSelector((state) => state.app.isMenuOpen);
+
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
+
+  const closeMenuHandler = () => {
+    dispatch(closeMenu());
+  };
 
   return (
     <>
       <div className="grid grid-flow-col p-3 mt-1 shadow-lg">
         <div className="flex col-span-1">
-          <button onClick={()=>toggleMenuHandler()} className="cursor-pointer">
-            <GiHamburgerMenu size={48}/>
+          <button onClick={isMenuOpen ? closeMenuHandler : toggleMenuHandler} className="cursor-pointer">
+            {isMenuOpen ? <ImCross size={48}/> : <GiHamburgerMenu size={48} />}
           </button>
+
           <img
             className="h-8 mx-4 mt-2"
             src="https://logodownload.org/wp-content/uploads/2014/10/youtube-logo-9.png"
@@ -42,9 +49,9 @@ const toggleMenuHandler = ()=>{
           <FaRegUser size={35} />
         </div>
       </div>
-      
     </>
   );
 };
 
 export default Head;
+
